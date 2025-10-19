2b603c4b6c4ab781f4808876b52362c17e27bf1b
nishb <nishbcodes@gmail.com>
2025-10-19 13:55:36 +0530

    async-handler wrapper util added

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,13 +1,18 @@
 import ApiResponse from "../utils/api-response.js";
+import asyncHandler from "../utils/asyn-handler.js";
 
-const healthCheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is Running" }));
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const healthCheck = (req, res) => {
+//   try {
+//     res
+//       .status(200)
+//       .json(new ApiResponse(200, { message: "Server is Running" }));
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is Running" }));
+});
 
 export default healthCheck;
